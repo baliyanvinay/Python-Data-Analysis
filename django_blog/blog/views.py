f8a710480d9f7fb5cdb2d8d3e1650f29365c5f56
Vinay Baliyan <baliyanvinay17@outlook.com>
2020-08-30 11:56:29 +0530

    Added a users app

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,24 +1,9 @@
 from django.shortcuts import render
-
-posts=[
-    {
-        'author': 'Vinay',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': '21-Aug-2020'
-        
-    },
-    {
-        'author': 'Sagar',
-        'title': 'Blog post 2',
-        'content': 'Other post content',
-        'date_posted': '01-Aug-2020'
-    }
-]
+from .models import Post
 
 def home(request):
     context={
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context) # render func returns HttpResponse
 
